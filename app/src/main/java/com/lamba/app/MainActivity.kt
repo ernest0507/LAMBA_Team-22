2514fccb4d58c5dfeecc6a55fbd907d18b501533
ernest0507 <ergoog51@gmail.com>
2026-06-12 23:10:29 +0300

    Add AI-chat screen mock

--- app/src/main/java/com/lamba/app/MainActivity.kt
+++ app/src/main/java/com/lamba/app/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.lamba.app.ui.theme.LAMBA_MVPv0Theme
+import com.lamba.app.screens.assistant.AiChatScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +20,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LAMBA_MVPv0Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                setContent {
+                    AiChatScreen()
                 }
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
             }
         }
     }
